15a3e846e7c8b3426017e0b05084c577cc2729ae
Jeremy Love <jeremy@tamarackmedia.com>
2023-12-10 17:56:26 -0800

    s3 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
           <p>Join out researchers </p>
 
           <div className="w-full mt-4">
-            {!isAuth ? (
+            {isAuth ? (
               <FileUpload />
             ) : (
               <Link href="/sign-in">
